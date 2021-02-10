847f27623182222654e6b672bd98d2431b0686ca
d-e-k-k <davideberhart8@gmail.com>
2021-02-10 14:02:36 -0500

    Removed commented out code

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,22 +1,3 @@
-
-// import React from "react";
-// import ReactDOM from "react-dom";
-// import "./App";
-// import "./index.css";
-// import App from "./App";
-// import reducer, { initialState } from "./reducer";
-// import { StateProvider } from "./StateProvider";
-// import { BrowserRouter as Router } from "react-router-dom";
-
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <StateProvider initialState={initialState} reducer={reducer}>
-//       <App />
-//     </StateProvider>
-//   </React.StrictMode>,
-//   document.getElementById("root")
-// );
-
 import React from "react";
 import ReactDOM from "react-dom";
 import "./App.css";
